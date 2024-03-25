ead731680ec837ed6ce3660726fbc12cafae63b9
bhabani <bhhabanibhabani2000@gmail.com>
2024-03-25 12:45:14 +0530

    this is the code that i conmmited at 12.43

---
+++ main.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    printf("this is a c language code")
+}
---
+++ main.py
@@ -0,0 +1,2 @@
+print("hello i am bhabani")
+print("i am from bhadrak but staying at bengaluru")
